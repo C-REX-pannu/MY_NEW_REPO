421e92d73400804b609816604d5c33803b763c28
C-REX <pannagapradyumn@gmail.com>
2022-12-15 21:51:52 +0530

    SAVED

---
+++ Program_1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <iomanip>
+#include <conio.h>
+#include "../../C++/Libraries/my_lib.h"
+#include "../../C++/Libraries/my_lib.cpp"
+using namespace std;
+using namespace std_func;
+using namespace math_func;
+
+int main()
+{
+
+    return 0;
+}
