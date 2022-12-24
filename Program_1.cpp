daa0dce7367a2589f1c11d2cacb5aec3630b9045
C-REX <pannagapradyumn@gmail.com>
2022-12-24 21:08:27 +0530

    Changed

--- Program_1.cpp
+++ Program_1.cpp
@@ -7,8 +7,24 @@ using namespace std;
 using namespace std_func;
 using namespace math_func;
 
+int largest(int a, int b, int c)
+{
+    int big;
+
+    big = a > b? (a > c? a : c) : (b > c? b : c);
+
+    return big;
+}
+
 int main()
 {
+    int a, b, c;
+    clrscr();
+
+    cout << "Enter any three numbers: ";
+    cin >> a >> b >> c;
+
+    cout << largest(a, b, c);
 
     return 0;
 }
