e6b433f23f5f8fad019d7482ada43870313426e8
C-REX <pannagapradyumn@gmail.com>
2022-12-26 20:13:56 +0530

    Changed

--- Program_1.cpp
+++ Program_1.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
 #include <iomanip>
 #include <conio.h>
-#include "../../C++/Libraries/my_lib.h"
-#include "../../C++/Libraries/my_lib.cpp"
+#include "../Libraries/my_lib.h"
+#include "../Libraries/my_lib.cpp"
 using namespace std;
 using namespace std_func;
 using namespace math_func;
