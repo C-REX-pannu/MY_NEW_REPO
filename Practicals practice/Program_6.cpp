e746c7244b3240b5301d755dc85de1aead1231f7
C-REX <pannagapradyumn@gmail.com>
2023-01-20 13:31:17 +0530

    changed

--- Practicals practice/Program_6.cpp
+++ Practicals practice/Program_6.cpp
@@ -1,15 +1,32 @@
-#include<iomanip>
-#include<iostream>
-#include<conio.h>
-#include<math.h>
-using namespace std;
+// EXAM PURPOSE ONLY.
 
-int main(){
-    double s, s1, s2, s3, a;
-    cout<<"Enter the lengths of the sides of the triangle: ";
-    cin>>s1>>s2>>s3;
-    s = (s1 + s2 + s3)/2;
-    a = sqrt(s *(s - s1)*(s - s2)*(s - s3));
-    cout<<"The are of the triangle is: "<<a;
+#include <iostream>
+#include "../Libraries/my_lib.h"
+#include "../Libraries/my_lib.cpp"
+using namespace std;
+using namespace std_func;
+using namespace math_func;
+
+int main()
+{
+    float amt, net_amt, net_dis;
+    clrscr();
+
+    cout << "Enter the total amout of your purchase : ";
+    cin >> amt;
+
+    net_dis = 0;
+
+    if (amt > 1000)
+    {
+        net_dis = amt * (8.00 / 100.00);
+    }
+
+    net_amt = amt - net_dis;
+
+    cout << "The total amount of your purchase is : " << amt << endl;
+    cout << "The discount availed is : " << net_dis << endl;
+    cout << "The final amount of your purchase is : " << net_amt;
+
     return 0;
 }
