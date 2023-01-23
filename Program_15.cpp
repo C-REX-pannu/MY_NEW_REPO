586d4969e7319c3faabd846f4a0d6cc3842aecfe
C-REX <pannagapradyumn@gmail.com>
2023-01-23 15:56:11 +0530

    yes

---
+++ Program_15.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include "../Libraries/my_lib.h"
+#include "../Libraries/my_lib.cpp"
+using namespace std;
+using namespace std_func;
+using namespace math_func;
+
+int main()
+{
+    int num, fact;
+    clrscr();
+
+    cout<<"Enter the number : ";
+    cin>>num;
+
+    fact = 1;
+
+    for (int i = 1; i <= num; i++)
+    {
+        fact = fact * i;
+    }
+    
+    cout<<"The faactorial of "<<num<<" is "<<fact;
+
+    return 0;
+
+}
